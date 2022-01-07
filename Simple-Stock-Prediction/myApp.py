c7e03b211b2fe6d08f7a54417c01751c3dd7f64d
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-01-07 22:40:28 +0545

    Google Stock Predict

---
+++ Simple-Stock-Prediction/myApp.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price Appp
+
+Shown are the stock **closing price** and ***volume*** of Google
+""")
+
+#Defines ticker symbol
+tickerSymbol = 'GOOGL'
+#get data  on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for the ticker
+
+tickerDf = tickerData.history(period='1d', start='2010-1-1',end='2022-1-1')
+#Open High LOw CLose Volume Dividends Stock splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
