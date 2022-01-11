729db89016f56df4e348c6c3c9465b3855bb9262
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-01-11 22:32:42 +0545

    Predicition Proability

--- IrisClassification/myApp.py
+++ IrisClassification/myApp.py
@@ -5,7 +5,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-#Simple Iris Flower Prediction App
+# Simple Iris Flower Prediction App
 
 This app predict the Iris flower type!
 """)
@@ -13,13 +13,29 @@ This app predict the Iris flower type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('sepal_length',4.3,7.9,5.4)
-    sepal_width = st.sidebar.slider('sepal_width',2.0,4.4,3.4)
-    petal_length = st.sidebar.slider('petal_length',1.0,6.9,1.3)
-    petal_width = st.sidebar.slider('petal_width',0.1,2.5,0.2)
+    sepal_length = st.sidebar.slider('Sepal_length',4.3,7.9,5.5)
+    sepal_width = st.sidebar.slider('Sepal_width',2.0,4.4,3.5)
+    petal_length = st.sidebar.slider('Petal_length',1.0,6.9,1.5)
+    petal_width = st.sidebar.slider('Petal_width',0.1,2.5,0.7)
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
             'petal_width': petal_width}
     features =pd.DataFrame(data, index=[0])
     return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+predictions = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
