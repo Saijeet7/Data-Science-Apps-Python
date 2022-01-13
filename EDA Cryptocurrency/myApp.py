7767938946e3e070d999279a4988991a2e6bdf98
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-01-13 11:36:33 +0545

    Webscrapping added

--- EDA Cryptocurrency/myApp.py
+++ EDA Cryptocurrency/myApp.py
@@ -32,4 +32,89 @@ col1.header('Input Options')
 ##Sidebar - Currency price unit
 currency_price_unit = col1.selectbox('Select currency for price',('USD','BTC','ETH'))
 
+#Web scrapping Coin market cap data
 
+def load_data():
+    cmc = requests.get("https://coinmarketcap.com")
+    soup = BeautifulSoup(cmc.content, "html.parser")
+
+    data = soup.find("script", id="__NEXT_DATA__", type="application/json")
+    coins = {}
+    coin_data = json.loads(data.contents[0])
+    listings = coin_data["props"]["initialState"]["cryptocurrency"]["listingLatest"][
+        "data"
+    ]
+
+    attributes = listings[0]["keysArr"]
+    index_of_id = attributes.index("id")
+    index_of_slug = attributes.index("slug")
+
+    for i in listings[1:]:
+        coins[str(i[index_of_id])] = i[index_of_slug]
+
+    coin_name = []
+    coin_symbol = []
+    market_cap = []
+    percent_change_1h = []
+    percent_change_24h = []
+    percent_change_7d = []
+    price = []
+    volume_24h = []
+
+    index_of_slug = attributes.index("slug")
+    index_of_symbol = attributes.index("symbol")
+
+    index_of_quote_currency_price = attributes.index(
+        f"quote.{currency_price_unit}.price"
+    )
+    index_of_quote_currency_percent_change_1h = attributes.index(
+        f"quote.{currency_price_unit}.percentChange1h"
+    )
+    index_of_quote_currency_percent_change_24h = attributes.index(
+        f"quote.{currency_price_unit}.percentChange24h"
+    )
+    index_of_quote_currency_percent_change_7d = attributes.index(
+        f"quote.{currency_price_unit}.percentChange7d"
+    )
+    index_of_quote_currency_market_cap = attributes.index(
+        f"quote.{currency_price_unit}.marketCap"
+    )
+    index_of_quote_currency_volume_24h = attributes.index(
+        f"quote.{currency_price_unit}.volume24h"
+    )
+
+    for i in listings[1:]:
+        coin_name.append(i[index_of_slug])
+        coin_symbol.append(i[index_of_symbol])
+
+        price.append(i[index_of_quote_currency_price])
+        percent_change_1h.append(i[index_of_quote_currency_percent_change_1h])
+        percent_change_24h.append(i[index_of_quote_currency_percent_change_24h])
+        percent_change_7d.append(i[index_of_quote_currency_percent_change_7d])
+        market_cap.append(i[index_of_quote_currency_market_cap])
+        volume_24h.append(i[index_of_quote_currency_volume_24h])
+
+    df = pd.DataFrame(
+        columns=[
+            "coin_name",
+            "coin_symbol",
+            "market_cap",
+            "percent_change_1h",
+            "percent_change_24h",
+            "percent_change_7d",
+            "price",
+            "volume_24h",
+        ]
+    )
+    df["coin_name"] = coin_name
+    df["coin_symbol"] = coin_symbol
+    df["price"] = price
+    df["percent_change_1h"] = percent_change_1h
+    df["percent_change_24h"] = percent_change_24h
+    df["percent_change_7d"] = percent_change_7d
+    df["market_cap"] = market_cap
+    df["volume_24h"] = volume_24h
+    return df
+
+
+df = load_data()
